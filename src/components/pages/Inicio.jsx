48a1483f0a442cd0c5deb0cbd356524890d191b9
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-08-02 15:59:24 -0300

    funcion para buscar productos

--- src/components/pages/Inicio.jsx
+++ src/components/pages/Inicio.jsx
@@ -2,13 +2,21 @@ import { Container, Row, Form } from "react-bootstrap";
 import CardProducto from "./producto/CardProducto";
 import { useState } from "react";
 
-const Inicio = ({productos}) => {
-  const [terminoBusqueda, setTerminoBusqueda] = useState("")
-  
-  const handleInputChange = (e) =>{
-    setTerminoBusqueda(e.target.value)
-  // guarda en el estado lo que se escribe
-  } 
+const Inicio = ({ productos }) => {
+  const [terminoBusqueda, setTerminoBusqueda] = useState("");
+
+  const handleInputChange = (e) => {
+    setTerminoBusqueda(e.target.value);
+    // guarda en el estado lo que se escribe
+  };
+
+  const productosFiltrados = productos.filter((producto) =>
+    producto.nombreProducto
+      .toLowerCase()
+      .includes(terminoBusqueda.toLowerCase())
+  );
+  // filtra el array de productos, agarra un producto por vez, y se fija que el termino en minuscula incluya lo que se escribio en el input
+  // devuelve true e false
 
   return (
     <section className="mainSection">
@@ -21,17 +29,29 @@ const Inicio = ({productos}) => {
         <h1 className="display-4">Nuestros Productos</h1>
         <hr />
         <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Buscar producto</Form.Label>
-        <Form.Control type="text" placeholder="Ingresa nombre del producto" onChange={handleInputChange} value={terminoBusqueda}/>
-      </Form.Group>
-
-    </Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Buscar producto</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="Ingresa nombre del producto"
+              onChange={handleInputChange}
+              value={terminoBusqueda}
+            />
+          </Form.Group>
+        </Form>
         <Row>
-          {
-            productos.map((producto)=> <CardProducto key={producto.id} producto={producto}></CardProducto>)
-          }
-          
+          {productosFiltrados.length > 0 ? (
+            productosFiltrados.map((producto) => (
+              <CardProducto
+                key={producto.id}
+                producto={producto}
+              ></CardProducto>
+            ))
+          ) : (
+            <p className="text-center my-5">
+              No se encontraron productos para mostrar
+            </p>
+          )}
         </Row>
       </Container>
     </section>
