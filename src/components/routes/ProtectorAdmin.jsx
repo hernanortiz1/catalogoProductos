54ccc3d96123796491f93dbb5ce492c33d7f199c
Hernan Ortiz <hernannicolasortiz@gmail.com>
2025-09-13 23:45:44 -0300

    corregi funcion Login

--- src/components/routes/ProtectorAdmin.jsx
+++ src/components/routes/ProtectorAdmin.jsx
@@ -1,17 +1,17 @@
 import { Navigate, Outlet } from "react-router";
 
-const ProtectorAdmin = ({isAdmin}) => {
-    //no es admin 
-    if(!isAdmin){
-        return <Navigate to={"/"}></Navigate>
-    }
+const ProtectorAdmin = ({ isAdmin }) => {
+  //no es admin
+  if (!isAdmin.token) {
+    return <Navigate to={"/"}></Navigate>;
+  }
 
-    // si es admin muestra las rutas
-    
-    return (
-        // se transforma en lo que necesita el admin
-        <Outlet />
-    );
+  // si es admin muestra las rutas
+
+  return (
+    // se transforma en lo que necesita el admin
+    <Outlet />
+  );
 };
 
 export default ProtectorAdmin;
